d774d4068cf004e796146b928ead7a6cd15cdedf
Sebastian Regalado <sdregala@uwaterloo.ca>
2023-11-13 23:08:30 -0500

    modify app to showcase merged sentences

--- app/streamlit.py
+++ app/streamlit.py
@@ -1,11 +1,10 @@
 import json
-from streamlit import st
+import streamlit as st
 from annotated_text import annotated_text
-from utils import DATA_DIR
+from utils import DATA_DIR, merge_sentences
  
 with open(DATA_DIR / "yahoo_health_medical_short_classified.json", encoding="utf-8") as f:
     data = json.load(f)
-questions = [q for q in data if "classified_answers" in q]
 
 if 'counter' not in st.session_state: 
     st.session_state.counter = 0
@@ -21,18 +20,19 @@ label2color = {
 def convert_answer(answer):
     return [(sentence, label, label2color.get(label, "#000000")) for sentence, label in answer]
 
-
 def show_question():
+    st.title("Multi Answer Summarization for CHF")
     index = st.session_state.counter
-    question = questions[index]
+    question = data[index]
     subject = question["subject"]
     content = question["content"]
     classified_answers = question["classified_answers"]
-    st.write(f"Subject: {subject}")
-    st.write(f"Content: {content}")
-    st.write("Answers:")
+    st.write(f"## Subject: {subject}")
+    st.write(f"## Content: {content}")
 
-    for answer in classified_answers:
+    for i, answer in enumerate(classified_answers):
+        st.write(f"Answer {i+1}")
+        answer = merge_sentences(answer)
         answer = convert_answer(answer)
         annotated_text(*answer)
 
@@ -40,5 +40,4 @@ def show_question():
     if st.session_state.counter >= len(questions):
         st.session_state.counter = 0
 
-st.title("Multi Answer Summarization for CHF")
 button = st.button("Next question", on_click=show_question)
--- utils.py
+++ utils.py
@@ -14,3 +14,21 @@ def get_device():
     else:
         device = "cpu"
     return torch.device(device)
+
+def merge_sentences(answer: list[tuple[str, str]]) -> list[tuple[str, str]]:
+    merged = []
+    prev_label = None
+    prev_sentence = None
+    for sentence, label in answer:
+        if prev_label is None:
+            prev_label = label
+            prev_sentence = sentence
+        elif prev_label == label:
+            prev_sentence += " " + sentence
+        else:
+            merged.append((prev_sentence, prev_label))
+            prev_label = label
+            prev_sentence = sentence
+    if prev_sentence is not None:
+        merged.append((prev_sentence, prev_label))
+    return merged
