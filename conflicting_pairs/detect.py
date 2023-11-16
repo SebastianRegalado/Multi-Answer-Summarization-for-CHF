0cf0849496190a16b79ce99c50ab3b988f2b57ef
Sebastian Regalado <sdregala@uwaterloo.ca>
2023-11-16 09:47:32 -0500

    generalize detection

--- conflicting_pairs/detect.py
+++ conflicting_pairs/detect.py
@@ -18,36 +18,25 @@ def load_data(file):
 def extract_perspective(answer, label):
     return [(s, i) for i, (s, persp) in enumerate(answer) if persp == label]
 
-labels = ["contradiction", "entailment", "neutral"]
-
-multi_nli = datasets.load_dataset("multi_nli")
-model = CrossEncoder("cross-encoder/nli-deberta-v3-base")
-
-data = load_data(FILE)
-output_file = DATA_DIR / "yahoo_health_medical_short_classified_contradiction.json"
-num_pairs = 0
-num_conflicting_pairs = 0
-for q in tqdm(data):
-    question = q["subject"]
-    all_causes = []
+def extract_sentences(q, label):
+    all_sentences = []
     for i, answer in enumerate(q["classified_answers"]):
-        causes = extract_perspective(answer, "cause")
-        causes = [(s, i, j) for s, j in causes]
-        all_causes.extend(causes)
+        sentences = extract_perspective(answer, label)
+        sentences = [(s, i, j) for s, j in sentences]
+        all_sentences.extend(sentences)
 
-    q["cause_contradiction_pairs"] = []
-    num_causes = len(all_causes)
-    for i in range(num_causes):
-        for j in range(i+1, num_causes):
-            num_pairs += 1
-            cause1, answer_idx1, cause_idx1 = all_causes[i]
-            cause2, answer_idx2, cause_idx2 = all_causes[j]
-            if cause1 == cause2:
-                continue
+    return all_sentences
+
+def extract_conflicting_pairs(all_sentences):
+    pairs = []
+    num_sentences = len(all_sentences)
+    for i in range(num_sentences):
+        for j in range(i+1, num_sentences):
+            cause1, answer_idx1, cause_idx1 = all_sentences[i]
+            cause2, answer_idx2, cause_idx2 = all_sentences[j]
             score = model.predict([cause1, cause2])
             label = labels[np.argmax(score)]
             if label == "contradiction":
-                num_conflicting_pairs += 1
                 pair = {
                     "cause1": cause1,
                     "cause2": cause2,
@@ -56,11 +45,38 @@ for q in tqdm(data):
                     "answer_idx2": answer_idx2,
                     "cause_idx2": cause_idx2,
                 }
-                q["cause_contradiction_pairs"].append(pair)
+                pairs.append(pair)
+    return pairs
 
+labels = ["contradiction", "entailment", "neutral"]
+
+multi_nli = datasets.load_dataset("multi_nli")
+model = CrossEncoder("cross-encoder/nli-deberta-v3-base")
+
+data = load_data(FILE)
+output_file = DATA_DIR / "yahoo_health_medical_short_classified_contradiction.json"
+
+target_labels = ["cause", "treatment"]
+
+from collections import defaultdict
+num_pairs = defaultdict(int)
+num_conflicting_pairs = defaultdict(int)
+for q in tqdm(data):
+    question = q["subject"]
+    for label in target_labels:
+        all_sentences = extract_sentences(q, label)
+        pairs = extract_conflicting_pairs(all_sentences)
+        q[f"{label}_contradiction_pairs"] = pairs
+        num_conflicting_pairs[label] += len(pairs)
+        num_sentences = len(all_sentences)
+        num_pairs[label] += num_sentences * (num_sentences - 1) / 2
+        
 with open(output_file, "w") as f:
     json.dump(data, f, indent=2)
 
-print(f"Number of pairs: {num_pairs}")
-print(f"Number of conflicting pairs: {num_conflicting_pairs}")
-print(f"Percentage of conflicting pairs: {num_conflicting_pairs/num_pairs*100:.2f}%")
+for label in target_labels:
+    print(f"Label: {label}")
+    print(f"Number of pairs: {num_pairs[label]}")
+    print(f"Number of conflicting pairs: {num_conflicting_pairs[label]}")
+    print(f"Percentage of conflicting pairs: {num_conflicting_pairs[label]/num_pairs[label]*100:.2f}%")
+
